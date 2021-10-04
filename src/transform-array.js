2ffa947590e45cfdca2ce315b6e2e4a3379fc020
Blackdoll-Loki <helpmeloki7@gmail.com>
2021-10-05 02:29:39 +0300

    skipped transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,28 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
+export default function transform( arr ) {
+  if(!Array.isArray(arr)){
+    return "\'arr\' parameter must be an instance of the Array!";
+  }
+  let resultArr = [];
+  for(let i = 0; i < arr.length; i++ ){
+    if(arr[i] === '--discard-prev'){
+      resultArr.splice(resultArr.length-1,1);
+      continue;
+    } else if(arr[i] === '--double-prev'){
+      resultArr.splice(resultArr.length-1, 0, resultArr[resultArr.length-1]);
+      continue;
+    } else if(arr[i] === 'discard-next'){
+      i = i + 2;
+      continue;
+    } else if(arr[i] === '--double-next'){
+      resultArr.push(arr[i+1],arr[i+1]);
+      i = i + 2;
+      continue;
+    } 
+      resultArr.push(arr[i]); 
+  }
+  return resultArr;
   // remove line with error and write your code here
 }
